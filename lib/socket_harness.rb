07c4a8359d17558099c1d51f6a0409b58a2ae739
wpc <alex.hal9000@gmail.com>
2014-03-14 10:31:36 -0700

    a better command line interface

--- lib/socket_harness.rb
+++ lib/socket_harness.rb
@@ -1,20 +1,42 @@
 require "socket_harness/version"
 require "socket_harness/proxy"
 require 'socket'
+require 'optparse'
 
 module SocketHarness
 
   def self.cli
-    if ARGV.length < 1
-      $stderr.puts "Usage: #{$0} remoteHost:remotePort [ localPort [ localHost ] ]"
+    options = {}
+    parser = OptionParser.new do |opts|
+      opts.banner = "Usage: #{$0} [options] remotehost remoteport"
+      opts.version = SocketHarness::VERSION
+
+      opts.on("-p", "--port PORT", "local port to bind") do |v|
+        options[:local_port] = v
+      end
+
+      opts.on("-b", "--bind ADDRESS", "local address to bind") do |v|
+        options[:local_host] = v
+      end
+
+      opts.on("--block-size NUMBER", "chunck out large data reads. by default 1024 byte") do |v|
+        options[:local_host] = v
+      end
+
+
+      opts.on("-d", "--delay NUMBER", "delay for client response: 12 will deplay 12ms for package sending back to the client.") do |v|
+        options[:delay] = v
+      end
+    end
+    parser.parse!
+    remote_host = ARGV.shift
+    remote_port = ARGV.shift
+
+    if remote_host.to_s.empty? || remote_port.to_s.empty?
+      puts parser
       exit 1
     end
 
-    remoteHost, remotePort = ARGV.shift.split(":")
-    puts "target address: #{remoteHost}:#{remotePort}"
-    localPort = ARGV.shift || '12121'
-    localHost = ARGV.shift
-
-    Proxy.new(remoteHost, remotePort, localHost, localPort).run
+    Proxy.new(remote_host, remote_port, options).run
   end
 end
--- lib/socket_harness/proxy.rb
+++ lib/socket_harness/proxy.rb
@@ -1,16 +1,24 @@
 module SocketHarness
   class Proxy
     BLOCK_SIZE = 1024
-    def initialize(remote_host, remote_port, local_host, local_port)
+
+    def self.default_options
+      {
+        :local_port => 13012,
+        :block_size => 1024,
+        :delay => 0,
+      }
+    end
+
+    def initialize(remote_host, remote_port, options={})
       @remote_host = remote_host
       @remote_port = remote_port
-      @local_host = local_host
-      @local_port = local_port
-      @delay = 2000
+      @options = self.class.default_options.merge(options)
     end
 
     def run
-      server = TCPServer.open(@local_host, @local_port.to_i)
+      puts "*** target #{@remote_host}:#{@remote_port}"
+      server = TCPServer.open(@options[:local_host], @options[:local_port].to_i)
 
       port = server.addr[1]
       addrs = server.addr[2..-1].uniq
@@ -40,30 +48,28 @@ module SocketHarness
       # open connection to remote server
       remote = TCPSocket.new(@remote_host, @remote_port)
 
-
-
       # start reading from both ends
       loop do
         ready = select([local, remote], nil, nil)
         if ready[0].include? local
           # local -> remote
-          data = local.recv(BLOCK_SIZE)
+          data = local.recv(@options[:block_size])
           if data.empty?
             puts "local end closed connection"
             break
           end
-          puts "#{name}:#{port} -{0}-> #{@remote_host}:#{@remote_port} #{data.size} byte ..."
+          puts "#{name}:#{port} -{}-> #{@remote_host}:#{@remote_port} #{data.size} byte ..."
           remote.write(data)
         end
         if ready[0].include? remote
           # remote -&gt; local
-          data = remote.recv(BLOCK_SIZE)
+          data = remote.recv(@options[:block_size])
           if data.empty?
             puts "remote end closed connection"
             break
           end
-          puts "#{@remote_host}:#{@remote_port} -{#{@delay}ms}-> #{name}:#{port}  #{data.size} byte ..."
-          sleep @delay * 1.0 / 1000.0
+          puts "#{@remote_host}:#{@remote_port} -{#{delay}ms}-> #{name}:#{port}  #{data.size} byte ..."
+          sleep delay * 1.0 / 1000.0
           local.write(data)
         end
       end
@@ -72,6 +78,10 @@ module SocketHarness
       remote.close
 
       puts "*** done with #{name}:#{port}"
+    end
+
+    def delay
+      @options[:delay].to_i
     end
   end
 end
